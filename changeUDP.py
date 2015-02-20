2b8d149e64955029c19388b4e3911801cf073100
agallo <agallo@gwu.edu>
2015-02-20 16:13:47 -0500

    correcting typos

---
+++ changeUDP.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+__author__ = 'agallo'
+
+
+'''
+the extension file must be a list of 5 digit extensions
+one per line.  no checking is done to ensure that the
+extensions valid dial plan extensions or even numbers
+it must always be called extfile.  the output is always
+extfile.ossi and will be overwritten if it exists
+'''
+extensions = open('extfile', 'r')
+outfile = open('extfile.ossi', 'w')
+
+# declare field addresses
+# these are the CM database addresses used to identify
+# different fields within CM forms
+matchingpattern = "f6c01ff01"
+length = "fec36ff01"
+delete = "fec37ff01"
+insert = "f6c02ff01"
+net    = "f6c03ff01"
+
+
+# reading extensions from the file:
+# splitlines was used rather than 'for each line in' because
+# that format included the newline character, which was
+# printed in the output file, resulting in an uneeded blank line
+# when reading with split lists, a list was created, and [0] retrieves
+# the first value
+
+
+for line in extensions:
+    currext = line.splitlines()[0]
+    print >> outfile, 'cchange uniform-dialplan', currext
+    print >> outfile, matchingpattern
+    print >> outfile, '%s%s' % ('d', currext)
+    print >> outfile, length
+    print >> outfile, "%s%s" % ('d', '5')
+    print >> outfile, delete
+    print >> outfile, "%s%s" % ('d', '1')
+    print >> outfile, insert
+    print >> outfile, "%s%s" % ('d', '142')
+    print >> outfile, net
+    print >> outfile, "%s%s" % ('d', 'aar')
+    print >> outfile, 't'
+    print >> outfile
+
+
+extensions.close()
+outfile.close()
