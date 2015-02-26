347b1a3f4502d9e7df5e872346421ca5bc87508f
agallo <agallo@gwu.edu>
2015-02-26 16:27:31 -0500

    initial release with sanity checking

--- changeUDP.py
+++ changeUDP.py
@@ -1,17 +1,32 @@
-#!/usr/bin/python
-
 __author__ = 'agallo'
 
 
-'''
-the extension file must be a list of 5 digit extensions
-one per line.  no checking is done to ensure that the
-entries are valid dial plan extensions or even numbers
-it must always be called extfile.  the output is always
-extfile.ossi and will be overwritten if it exists
-'''
-extensions = open('extfile', 'r')
-outfile = open('extfile.ossi', 'w')
+from argparse import ArgumentParser
+import os
+
+parser = ArgumentParser(description="Script to generate an OSSI file for mass"
+                                    " Uniform Dialing Plan (UDP) changes")
+
+
+parser.add_argument('-i', '--infile', dest='extfile',
+                    help="specify input file. Default is extfile",
+                    default='extensions')
+parser.add_argument('-o', '--outfile', dest='outfile',
+                    help="specify output file. Default is extfile.OSSI",
+                    default='extfile.OSSI')
+parser.add_argument('-c', '--checktype', dest='checktype',
+                    help='Sanity checking type. Default is strict (die if file is not clean)',
+                    default='strict')
+args = parser.parse_args()
+
+
+extensions = open(args.extfile, 'r')
+outfile = open(args.outfile, 'w')
+
+sanity = args.checktype
+
+failcount = 0
+successcount = 0
 
 # declare field addresses
 # these are the CM database addresses used to identify
@@ -23,6 +38,18 @@ insert = "f6c02ff01"
 net    = "f6c03ff01"
 
 
+def extsanitycheck():
+    global failcount
+    counter = 1
+    for line in extensions:
+        try:
+            int(line)
+        except ValueError:
+            failcount += 1
+        counter += 1
+    return failcount
+
+
 # reading extensions from the file:
 # splitlines was used rather than 'for each line in' because
 # that format included the newline character, which was
@@ -31,22 +58,54 @@ net    = "f6c03ff01"
 # the first value
 
 
-for line in extensions:
-    currext = line.splitlines()[0]
-    print >> outfile, 'cchange uniform-dialplan', currext
-    print >> outfile, matchingpattern
-    print >> outfile, '%s%s' % ('d', currext)
-    print >> outfile, length
-    print >> outfile, "%s%s" % ('d', '5')
-    print >> outfile, delete
-    print >> outfile, "%s%s" % ('d', '1')
-    print >> outfile, insert
-    print >> outfile, "%s%s" % ('d', '142')
-    print >> outfile, net
-    print >> outfile, "%s%s" % ('d', 'aar')
-    print >> outfile, 't'
-    print >> outfile
+def createOSSI():
+    extensions.seek(0)
+    currentline = 0
+    global successcount
+    global failcount
+    failcount = 0
+    for line in extensions:
+        currentline += 1
+        currext = line.splitlines()[0]
+        try:
+            int(currext)
+            print >> outfile, 'cchange uniform-dialplan', currext
+            print >> outfile, matchingpattern
+            print >> outfile, '%s%s' % ('d', currext)
+            print >> outfile, length
+            print >> outfile, "%s%s" % ('d', '5')
+            print >> outfile, delete
+            print >> outfile, "%s%s" % ('d', '1')
+            print >> outfile, insert
+            print >> outfile, "%s%s" % ('d', '142')
+            print >> outfile, net
+            print >> outfile, "%s%s" % ('d', 'aar')
+            print >> outfile, 't'
+            print >> outfile
+            successcount += 1
+        except:
+            failcount += 1
 
+    return successcount, failcount
+
+
+def main():
+    extsanitycheck()
+    if failcount > 0 and sanity == 'strict':
+        print
+        print '****not creating OSSI file because it isn\'t clean.'
+        print 'There are ', failcount, 'errors in the file.\nPlease correct them.'
+        outfile.close()
+        os.remove(args.outfile)
+    else:
+        createOSSI()
+        print "Processed ", successcount, ' extensions.'
+        print failcount, "lines were skipped because of errors on the line."
+        print "OSSI file is ", args.outfile
+
+
+
+main()
 
 extensions.close()
 outfile.close()
